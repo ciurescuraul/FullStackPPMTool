deb21c8aa67565b32cc0a4bba2d97120e399197a
Raul Ciurescu <raul.ciurescu@computaris.com>
2021-04-13 18:00:28 +0300

    added binding result Object and extracted field errors from it

--- backend/src/main/java/com/rsystems/ppmtool/web/ProjectController.java
+++ backend/src/main/java/com/rsystems/ppmtool/web/ProjectController.java
@@ -1,8 +1,14 @@
 package com.rsystems.ppmtool.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.rsystems.ppmtool.domain.Project;
 import com.rsystems.ppmtool.services.ProjectService;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -27,11 +31,21 @@ public class ProjectController
 
   @PostMapping
   public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project,
-                                                  BindingResult result)
+                                            BindingResult result)
   {
-    if (result.hasErrors()){
-      return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+    if (result.hasErrors())
+    {
+
+      Map<String, String> errorMap = new HashMap<>();
+      for (FieldError error : result.getFieldErrors())
+      {
+        errorMap.put(error.getField(), error.getDefaultMessage());
+      }
+
+      return new ResponseEntity<Map<String, String>>(errorMap,
+                                                     HttpStatus.BAD_REQUEST);
     }
+
     Project project1 = projectService.saveOrUpdateProject(project);
     return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
   }
