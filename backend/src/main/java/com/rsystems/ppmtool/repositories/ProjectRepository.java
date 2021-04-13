1f2c69335a10acf8054bf0e5314fc4fdd34f02a2
Raul Ciurescu <raul.ciurescu@computaris.com>
2021-04-13 13:40:05 +0300

    added project repository and project entity

---
+++ backend/src/main/java/com/rsystems/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.rsystems.ppmtool.repositories;
+
+import com.rsystems.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>
+{
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
