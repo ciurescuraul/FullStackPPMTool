c103720cae8995a2154276f43bfd93698dea22f1
Raul Ciurescu <raul.ciurescu@computaris.com>
2021-04-13 14:03:57 +0300

    replaced autowire injection with constructor injection in project controller

--- backend/src/main/java/com/rsystems/ppmtool/web/ProjectController.java
+++ backend/src/main/java/com/rsystems/ppmtool/web/ProjectController.java
@@ -1,8 +1,5 @@
 package com.rsystems.ppmtool.web;
 
-import com.rsystems.ppmtool.domain.Project;
-import com.rsystems.ppmtool.services.ProjectService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,13 +7,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rsystems.ppmtool.domain.Project;
+import com.rsystems.ppmtool.services.ProjectService;
+
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController
 {
 
-  @Autowired
   private ProjectService projectService;
+
+  public ProjectController(ProjectService projectService) {
+    this.projectService = projectService;
+  }
 
   @PostMapping("")
   public ResponseEntity<Project> createNewProject(@RequestBody Project project)
