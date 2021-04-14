1a7252951cbfd6f5cd078933192e84067375b0f2
Raul Ciurescu <raul.ciurescu@computaris.com>
2021-04-14 10:27:47 +0300

    added frontend and React Components

---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ProjectItem from "./Project/ProjectItem";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Welcome to the Dashboard</h1>
+      <ProjectItem />
+      <ProjectItem />
+      <ProjectItem />
+    </div>
+  );
+};
+
+export default Dashboard;
