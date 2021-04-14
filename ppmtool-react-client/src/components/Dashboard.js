1ea347e9e35cdd1d659d6e97ab2544065b37b59b
Raul Ciurescu <raul.ciurescu@computaris.com>
2021-04-14 10:37:38 +0300

    import and test bootstrap

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -4,7 +4,7 @@ import ProjectItem from "./Project/ProjectItem";
 const Dashboard = () => {
   return (
     <div>
-      <h1>Welcome to the Dashboard</h1>
+      <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
       <ProjectItem />
       <ProjectItem />
       <ProjectItem />
